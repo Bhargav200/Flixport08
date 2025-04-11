19aaffeed243fd545f90c478ca35ea2880815e18
A P BHARGAV RAVI TEJA <68964837+Bhargav200@users.noreply.github.com>
2025-04-11 14:30:27 +0530

    Update SocialIcons.tsx
    links updated

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -5,7 +5,7 @@ const SocialIcons = () => {
   return (
     <div className="fixed bottom-0 left-0 md:right-6 md:top-1/2 md:left-auto md:bottom-auto md:transform md:-translate-y-1/2 flex md:flex-col gap-6 z-50 bg-black/50 md:bg-transparent p-4 md:p-0 w-full md:w-auto justify-center">
       <a
-        href="https://github.com"
+        href="https://github.com/Bhargav200"
         target="_blank"
         rel="noopener noreferrer"
         className="text-white hover:text-green-400 transition-colors duration-300"
@@ -13,7 +13,7 @@ const SocialIcons = () => {
         <Github size={24} />
       </a>
       <a
-        href="https://linkedin.com"
+        href="https://www.linkedin.com/in/bhargavraviteja/"
         target="_blank"
         rel="noopener noreferrer"
         className="text-white hover:text-green-400 transition-colors duration-300"
